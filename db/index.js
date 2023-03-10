524b4ff11194456bca3cb6f801fc036f0f599fae
Yangqun123456 <2537148609@qq.com>
2023-03-10 19:55:34 +0800

    iot_project_3.10

--- db/index.js
+++ db/index.js
@@ -3,8 +3,8 @@ const mysql = require('mysql')
 const db = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
-  password: '12345678',
-  database: 'wxdb_01',
+  password: '123456',
+  database: 'iot_project_01',
   timezone: "Asia/Shanghai"
 })
 
